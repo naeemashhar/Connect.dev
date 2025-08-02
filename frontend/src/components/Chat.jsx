07584323c11c4ad13caffb203b403656f41a0f96
naeemashhar <naeemashhar98@gmail.com>
2025-08-03 02:09:43 +0530

    Light-Theme-DONE

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -19,6 +19,12 @@ const Chat = () => {
   const [showSidebar, setShowSidebar] = useState(false);
   const messagesEndRef = useRef(null);
 
+  const [isLightMode, setIsLightMode] = useState(true);
+   useEffect(() => {
+      const theme = localStorage.getItem("theme") || "light";
+      setIsLightMode(theme === "light");
+    }, []);
+
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
@@ -92,11 +98,15 @@ const Chat = () => {
   const targetUser = connections.find((c) => c._id === targetUserId);
 
   return (
-    <div className="w-full flex justify-center bg-radial from-cyan-500/25 to-[#020013]">
+    <div className={`w-full h-fit flex justify-center ${
+        isLightMode
+          ? "bg-radial from-[#feffff] to-[#ccd2dc]"
+          : "bg-radial from-cyan-500/25 to-[#020013]"
+      } `}>
       <div className="flex flex-col md:flex-row h-[90vh] m-2 md:m-5 border border-base-300 rounded-lg shadow-xl overflow-hidden w-full max-w-full">
         {/* Sidebar */}
         <aside
-          className={`w-full md:w-1/3 lg:w-1/4 bg-[#020013] p-4 border-b md:border-b-0 md:border-r border-base-300 overflow-y-auto transition-transform duration-300 ease-in-out z-10 md:translate-x-0 ${
+          className={`w-full md:w-1/3 lg:w-1/4  bg-[#F2F7FE] dark:bg-[#020013] p-4 border-b md:border-b-0 md:border-r border-base-300 overflow-y-auto transition-transform duration-300 ease-in-out z-10 md:translate-x-0 ${
             showSidebar ? "translate-x-0" : "-translate-x-full md:translate-x-0"
           } fixed md:static top-0 left-0 h-full md:h-auto`}
         >
@@ -147,7 +157,7 @@ const Chat = () => {
         {/* Chat Area */}
         <main className="flex-1 flex flex-col justify-between min-h-0">
           {/* Header */}
-          <div className="flex justify-between items-center p-4 border-b border-base-300 bg-base-100 sticky top-0 z-20">
+          <div className="flex justify-between items-center p-4 border-b dark:border-base-300 border-base-200 bg-[#F2F7FE] dark:bg-base-100 sticky top-0 z-20">
             <div className="flex items-center gap-3">
               <Link to={"/connections"}>
                 <ArrowLeft className="w-5 h-5" />
@@ -207,10 +217,10 @@ const Chat = () => {
                     </div>
                     
                     <div
-                      className={`max-w-[100%] md:max-w-[80%] px-3 py-1 rounded-tl-md rounded-br-md ${
+                      className={`max-w-[100%] md:max-w-[80%] px-4 py-2 rounded-tl-md rounded-br-md ${
                         isCurrentUser
                           ? "bg-cyan-600 text-gray-100 ml-auto rounded-bl-md"
-                          : "bg-base-300 text-base-content mr-auto rounded-tr-md"
+                          : "bg-base-200 dark:bg-base-300 text-base-content mr-auto rounded-tr-md"
                       }`}
                     >
                       {msg.text}
@@ -225,7 +235,7 @@ const Chat = () => {
           </div>
 
           {/* Input */}
-          <div className="p-4 border-t border-base-300 bg-[#020013] backdrop-blur-md">
+          <div className="p-4 border-t border-base-300 bg-[#F2F7FE] dark:bg-[#020013] backdrop-blur-md">
             <div className="flex justify-between items-center gap-2">
               <input
                 type="text"
@@ -237,7 +247,7 @@ const Chat = () => {
               />
               <button
                 onClick={sendMessage}
-                className="btn bg-[#0d1128] rounded-full px-4 py-2 hover:border-2 hover:border-cyan-500 hover:scale-105 transition"
+                className="btn bg-[#d3d8de] dark:bg-[#0d1128] rounded-full px-4 py-2 hover:border-2 hover:border-cyan-500 hover:scale-105 transition"
               >
                 <Send size={18} />
               </button>
